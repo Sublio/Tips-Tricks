e6ef8c5a82e06a78142a5e4b62452b984dc0417c
Sublio <sublio1260@gmail.com>
2016-10-29 18:23:49 +0600

    DMUtils

---
+++ TricksTest/TricksTest/DMUtils.h
@@ -0,0 +1,10 @@
+//
+//  DMUtils.h
+//  TricksTest
+//
+//  Created by sublio on 29/10/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
---
+++ TricksTest/TricksTest/DMUtils.m
@@ -0,0 +1,10 @@
+//
+//  DMUtils.m
+//  TricksTest
+//
+//  Created by sublio on 29/10/16.
+//  Copyright (c) 2016 sublio. All rights reserved.
+//
+
+#import "DMUtils.h"
+
